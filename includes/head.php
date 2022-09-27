787694550fc43d600f944517f56b44fb588092d2
VALMIR-DE-OLIVEIRA-FILHO <mirinho3789@gmail.com>
2022-09-27 12:19:28 -0300

    TSE0.1

---
+++ includes/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+
+  <header>
+    <img id="logo" src="./contents/TSE-logo.png" alt="">
+    <p>teste</p>
+
+  </header>
--- index.php
+++ index.php
@@ -1,60 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <main>
-            <section>
-                <div class="tela"></div>
-            </section>
-            <section>
-                <div class="teclado">
-                    <div class="col titulo">
-                        <img id="je" src="./imgs/justiçaeleitoral.png">
-                        <h3 id="tituloaa">Justiça Eleitoral</h3> 
-                    </div>
-                    <div class="container">
-
-                        <?php
-                        $aa = [
-                            0,
-                            1,
-                            2,
-                            3,
-                            4,
-                            5,
-                            6,
-                            7,
-                            8,
-                            9
-                        ];
-                        for ($i=0; $i <=8 ; $i++) {  ?>
-                            <button class="botao"><?php echo $aa[$i] + 1; ?></button>
-                            
-                        <?php
-                            }
-                        ?>
-                    </div>
-                        <div class="row">
-                            <div class="col">
-                                <button class="botao">0</button>
-                            </div>
-                        </div>    
-                        <div class="decisao">
-                            <button id="branco">Branco</button>
-                            <button id="apaga">Apaga</button>
-                            <button id="confirma">Confirma</button>
-                        </div>
-                    </div>
-                </div>
-
-            </section>
-        </div>
-    </main>
-</body>
-</html>
+<?php
+include_once "./includes/head.php";
+?>
+<div id="cadastro" class="container">
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-9">
+      <label for="inputNomeCompleto">Nome completo do candidato</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Nome...">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="inputEmail14"> partido</label>
+      <input type="text" class="form-control" id="inputPardido">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Vice candidato</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Rua dos Bobos, nº 0">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Endereço 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartamento, hotel, casa, etc.">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">Cidade</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputEstado">Estado</label>
+      <select id="inputEstado" class="form-control">
+        <option selected>Escolher...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputCEP">Idade</label>
+      <input type="text" class="form-control" id="inputCEP">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputCEP">CEP</label>
+      <input type="text" class="form-control" id="inputCEP">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Clique em mim
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Entrar</button>
+</form>
+</div>
+<?php
+include_once"./includes/footer.php";
+?>
